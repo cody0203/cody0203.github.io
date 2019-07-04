8bde21abc37f14b0ce23eb228e15732a98df355e
Cody <cody@Codys-iMac.local>
2019-07-04 18:54:38 +0700

    fix display suggest container

--- bootstrap_project/cut-html-css/main.js
+++ bootstrap_project/cut-html-css/main.js
@@ -4,10 +4,12 @@ function changeUi() {
     let sideBar = $('.sidebar').addClass('small');
     if (window.matchMedia && window.matchMedia('(max-width: 768px)').matches) {
         $('.single-posts').append(sideBar);
+        $('.suggest-items').addClass('small');
     } else {
         sideBar.remove();
-        $(sideBar).insertAfter('.main-content .container .posts-content')
-        $(sideBar).removeClass('small')
+        $(sideBar).insertAfter('.main-content .container .posts-content');
+        $(sideBar).removeClass('small');
+        $('.suggest-items').removeClass('small');
     }
     if (window.matchMedia && window.matchMedia('(max-width: 320px)').matches) {
         $('.comment-textbox').css('padding-left', '5px');
