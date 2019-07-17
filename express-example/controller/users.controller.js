98cbc99803b6bad179297532c245327a375507aa
Cody <cody@Codys-iMac.local>
2019-07-17 14:47:20 +0700

    update controller (MVC)

---
+++ express-example/controller/users.controller.js
@@ -0,0 +1,44 @@
+const db = require('../db')
+const shortid = require('shortid');
+
+module.exports = {
+    index: (req, res) => {
+        res.render('users/index', {
+            users: db.get('users').value()
+        })
+    },
+    search: (req, res) => {
+        let q = req.query.q;
+        let existedName;
+        let matchedUsers = db.get('users').value().filter(user => {
+            return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+        })
+        if (matchedUsers.some(item => item !== {})) {
+            existedName = true;
+        } else {
+            existedName = false
+        }
+        res.render('users/index', {
+            users: matchedUsers,
+            q,
+            existedName
+        })
+    },
+    getCreate: (req, res) => {
+        res.render("users/create")
+    },
+    view: (req, res) => {
+        let id = req.params.id;
+        let user = db.get('users')
+            .find({ id: id })
+            .value()
+        res.render("users/view", {
+            user: user
+        })
+    },
+    postCreate: (req, res) => {
+        req.body.id = shortid.generate();
+        db.get('users').push(req.body).write();
+        res.redirect('/users');
+    }
+};
--- express-example/routes/users.route.js
+++ express-example/routes/users.route.js
@@ -1,52 +1,15 @@
 let express = require('express')
 let routes = express();
-const shortid = require('shortid');
+let controller = require("../controller/users.controller")
 
-let db = require('../db')
+routes.get('/', controller.index);
 
-routes.get('/', (req, res) => {
-    res.render('users/index', {
-        users: db.get('users').value()
-    })
-}
-);
+routes.get('/search', controller.search);
 
-routes.get('/search', (req, res) => {
-    let q = req.query.q;
-    let existedName;
-    let matchedUsers = db.get('users').value().filter(user => {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    })
-    if (matchedUsers.some(item => item !== {})) {
-        existedName = true;
-    } else {
-        existedName = false
-    }
-    res.render('users/index', {
-        users: matchedUsers,
-        q,
-        existedName
-    })
-});
+routes.get("/create", controller.getCreate);
 
-routes.get("/create", (req, res) => {
-    res.render("users/create")
-});
+routes.get("/:id", controller.view);
 
-routes.get("/:id", (req, res) => {
-    let id = req.params.id;
-    let user = db.get('users')
-        .find({ id: id })
-        .value()
-    res.render("users/view", {
-        user: user
-    })
-})
-
-routes.post("/create", (req, res) => {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-})
+routes.post("/create", controller.postCreate);
 
 module.exports = routes;
