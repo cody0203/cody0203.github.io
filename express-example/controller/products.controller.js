3071e96852d224fb04e0b871ce071f98f52d9df7
Cody <cody@Codys-iMac.local>
2019-07-18 14:21:35 +0700

    update pagination for products showing

--- express-example/controller/products.controller.js
+++ express-example/controller/products.controller.js
@@ -3,8 +3,16 @@ const shortid = require('shortid');
 
 module.exports = {
     get: (req, res) => {
+        let page = parseInt(req.query.page) || 1;
+        let perPage = 8;
+        let maxPage = Math.ceil(db.get("products").value().length / perPage)
+        console.log(maxPage);
+        let start = (page - 1) * perPage;
+        let end = page * perPage;
         res.render('products/index', {
-            products: db.get("products").value()
+            products: db.get("products").value().slice(start, end),
+            page: page,
+            maxPage: maxPage
         });
     },
     view: (req, res) => {
