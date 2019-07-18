1b22776c1d825315598e651d7a902bffe9098645
Cody <cody@Codys-iMac.local>
2019-07-18 10:34:18 +0700

    update variable enviroment

--- express-example/main.js
+++ express-example/main.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -13,7 +15,7 @@ app.set('views', './views');
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser("dsofpadsjfpijasdpfjds"));
+app.use(cookieParser(process.env.SESSION_SECURE));
 
 let name = "Cody";
 
