cd0f220440f1f6404f1befe1790acbb1e5954dd8
Cody <cody@Codys-iMac.local>
2019-08-21 14:30:00 +0700

    update post page

---
+++ obo-stadium/script/lastest-news.js
@@ -0,0 +1,40 @@
+function carousel() {
+    $('.carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        appendArrows: $('.latest-news .title .slick-arrow'),
+        centerMode: true,
+        focusOnSelect: true,
+        variableWidth: true,
+        touchThreshold: 6,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    centerMode: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+};
--- obo-stadium/script/main.js
+++ obo-stadium/script/main.js
@@ -1,3 +1,9 @@
+$(function () {
+    changeUiHomePage();
+    carousel();
+})
+
+
 function changeUiHomePage() {
     if (window.matchMedia && window.matchMedia('(max-width: 768px)').matches) {
         $('#promo-banner-1').attr("src", "./image/banner/promo-banner-1-md.png");
@@ -23,50 +29,4 @@ function changeUiHomePage() {
     }
 }
 
-$(function () {
-    changeUiHomePage();
-    carousel();
-})
-
 $(window).resize(changeUiHomePage);
-
-function carousel() {
-    $('.carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-        centerMode: true,
-        focusOnSelect: true,
-        variableWidth: true,
-        touchThreshold: 6,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    centerMode: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
-    
-}
-
