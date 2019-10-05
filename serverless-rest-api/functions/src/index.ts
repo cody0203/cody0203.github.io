cad43ca091045a9f72ac6f442343e3b3bd0c97cb
Cody <cody@Thuy.local>
2019-10-05 11:14:13 +0700

    update cors

--- serverless-rest-api/functions/src/index.ts
+++ serverless-rest-api/functions/src/index.ts
@@ -5,14 +5,21 @@ import * as express from 'express';
 import * as bodyParser from "body-parser";
 const cors = require('cors');
 
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+};
+
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
 const app = express();
 const main = express();
 
-app.use(cors({ origin: true }));
-main.use(cors({ origin: true }));
+app.use(cors({ corsOptions }));
+main.use(cors({ corsOptions }));
 
 main.use('/api/v1', app);
 main.use(bodyParser.json());
@@ -31,14 +38,14 @@ interface Student {
 }
 
 // Add new student
-app.post('/students', async (req, res) => {
+app.post('/students', cors(corsOptions), async (req, res) => {
     try {
         const student: Student = {
             name: req.body['name'],
             birthYear: req.body['birthYear'],
             email: req.body['email'],
             phone: req.body['phone'],
-            created: Math.round(+new Date()/1000),
+            created: Math.round(+new Date() / 1000),
         }
 
         const newDoc = await firebaseHelper.firestore
@@ -46,18 +53,18 @@ app.post('/students', async (req, res) => {
         res.status(201).send(`Created a new student: ${newDoc.id}`);
     } catch (error) {
         res.status(400).send(`Student should only contains name, birth year, email and phone!!!`)
-    }        
+    }
 })
 
 // Update new student
-app.put('/students/:studentId', async (req, res) => {
+app.put('/students/:studentId', cors(corsOptions), async (req, res) => {
     const updatedDoc = await firebaseHelper.firestore
         .updateDocument(db, studentsCollection, req.params.studentId, req.body);
     res.status(204).send(`Update a new student: ${updatedDoc}`);
 })
 
 // View a student
-app.get('/students/:studentId', (req, res) => {
+app.get('/students/:studentId', cors(corsOptions), (req, res) => {
     firebaseHelper.firestore
         .getDocument(db, studentsCollection, req.params.studentId)
         .then(doc => res.status(200).send(doc))
@@ -65,7 +72,7 @@ app.get('/students/:studentId', (req, res) => {
 })
 
 // View all students
-app.get('/students', (req, res) => {
+app.get('/students', cors(corsOptions), (req, res) => {
     firebaseHelper.firestore
         .backup(db, studentsCollection)
         .then(data => res.status(200).send(data))
@@ -73,7 +80,7 @@ app.get('/students', (req, res) => {
 })
 
 // Delete a student 
-app.delete('/students/:studentId', async (req, res) => {
+app.delete('/students/:studentId', cors(corsOptions), async (req, res) => {
     const deleteStudent = await firebaseHelper.firestore
         .deleteDocument(db, studentsCollection, req.params.studentId);
     res.status(204).send(`Student is deleted: ${deleteStudent}`);
