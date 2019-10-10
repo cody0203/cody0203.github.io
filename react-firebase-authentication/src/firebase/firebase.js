4eb2b774d28f63a6506124ae99923d5ccfd3a916
cody0203 <congdinh2395@gmail.com>
2019-10-10 18:56:48 +0700

    remove react-firebase-authentication folder

--- react-firebase-authentication/src/firebase/firebase.js
+++
@@ -1,8 +0,0 @@
-import * as firebase from "firebase";
-import { devConfig } from "./config";
-
-!firebase.apps.length && firebase.initializeApp(devConfig);
-
-const auth = firebase.auth();
-
-export { auth };
