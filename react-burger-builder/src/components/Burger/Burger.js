9f8d2925b67175616e2ea1cbd9b976a58d0b7b71
cody0203 <congdinh2395@gmail.com>
2019-10-11 21:38:16 +0700

    update burger builder

--- react-burger-builder/src/components/Burger/Burger.js
+++ react-burger-builder/src/components/Burger/Burger.js
@@ -1,21 +1,27 @@
 import React from "react";
 import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
-import classes from "./Burger.css"
+import classes from "./Burger.css";
 
-const burger = (props) => {
-  const transformIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, index) => {
-      return <BurgerIngredients key={igKey + index} type={igKey} />
+const burger = props => {
+  let transformIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredients key={igKey + index} type={igKey} />;
+      });
     })
-  })
-  console.log(transformIngredients)
-  return(
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformIngredients.length === 0) {
+    transformIngredients = <p>Please start adding ingredients!</p>;
+  }
+  return (
     <div className={classes.Burger}>
       <BurgerIngredients type="bread-top" />
       {transformIngredients}
       <BurgerIngredients type="bread-bottom" />
     </div>
-  )
-}
+  );
+};
 
 export default burger;
--- react-burger-builder/src/components/Layout/Layout.js
+++ react-burger-builder/src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
-import classes from "./Layout.css"
+import classes from "./Layout.css";
 
 const layout = props => (
   <Aux>
