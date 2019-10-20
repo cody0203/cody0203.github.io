77e51c477a88abf126bb3e8f8d8c1942873668b3
cody0203 <congdinh2395@gmail.com>
2019-10-20 21:24:47 +0700

    update portfolio responsive

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 $(window).resize(changeUiFirstPart);
 
 function changeUiFirstPart() {
-    if (window.matchMedia && window.matchMedia('(max-width: 767px)').matches) {
-        $('#first-part').removeClass('fixed-position');
-    } else {
-        $('#first-part').addClass('fixed-position');
-    }
-    if (window.matchMedia && window.matchMedia('(max-width: 320px)').matches) {
-        $('.content').removeClass('ml-5');
-        $('.content').addClass('justify-content-center');
-    }
-};
+  if (window.matchMedia && window.matchMedia("(max-width: 768px)").matches) {
+    $("#first-part").removeClass("fixed-position");
+  } else {
+    $("#first-part").addClass("fixed-position");
+  }
+  if (window.matchMedia && window.matchMedia("(max-width: 320px)").matches) {
+    $(".content").removeClass("ml-5");
+    $(".content").addClass("justify-content-center");
+  }
+}
 changeUiFirstPart();
 
 function autoTyping() {
-    let typed = new Typed(".typing", {
-        strings: [` `, `Hi, I'm Cody`],
-        typeSpeed: 40,
-        showCursor: false
-    });
+  let typed = new Typed(".typing", {
+    strings: [` `, `Hi, I'm Cody`],
+    typeSpeed: 40,
+    showCursor: false
+  });
 
-    let typed2 = new Typed(".typing2", {
-        strings: [` `, `Front-end website developer based in Hanoi`],
-        typeSpeed: 40,
-        startDelay: 1500,
-        showCursor: false
-    });
+  let typed2 = new Typed(".typing2", {
+    strings: [` `, `Front-end website developer based in Hanoi`],
+    typeSpeed: 40,
+    startDelay: 1500,
+    showCursor: false
+  });
 }
 autoTyping();
 
@@ -254,7 +254,6 @@ autoTyping();
 // });
 
 // var ctx = document.getElementById('myChart').getContext('2d');
-
 
 // var color = Chart.helpers.color;
 // var config = {
